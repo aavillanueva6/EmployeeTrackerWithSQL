8d36d06a52601d1835a4e0373a0ae52c7eca88e2
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-10 22:47:04 -0700

    adds ascii logo and rearranges init funciton call

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // add dependencies
-const inquirer = require('inquirer');
-const sql = require('mysql2');
+// const inquirer = require('inquirer');
+// const sql = require('mysql2');
+const logo = require('asciiart-logo');
+// const config = require('./package.json');
 
 // add custom modules
-const init = require('./lib/InitPrompt');
+const startPrompts = require('./lib/InitPrompt');
+
+function init() {
+  console.log(
+    logo({
+      name: 'Employee Tracker',
+      lineChars: 0,
+      font: 'Fender',
+    }).render()
+  );
+  startPrompts();
+}
 
 init();
