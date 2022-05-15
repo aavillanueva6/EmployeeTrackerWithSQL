d9df5fee551aaec003fbb28e0ced94875a027e21
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-14 23:12:33 -0700

    add sql connection to config directory, to modularize connection

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+// add dependencies
+const inquirer = require('inquirer');
+const sql = require('mysql2');
+
+// open connection to sql server
+const db = sql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'pass',
+  database: 'employees_db',
+});
+module.exports = db;
